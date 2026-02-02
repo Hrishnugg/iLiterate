d4bc6be7ec7d2c33f3d9dc79940100d0bc72463d
cxl1503 <cxl1503@case.edu>
2026-02-01 20:32:46 -0500

    Update Flashcards page.tsx
    Allowing users to choose to view all cards, recently added cards, or create new cards

--- iliterate/src/app/(dashboard)/flashcards/page.tsx
+++ iliterate/src/app/(dashboard)/flashcards/page.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
 export default function FlashcardsPage() {
   return (
     <div>
@@ -5,6 +18,31 @@ export default function FlashcardsPage() {
       <p className="text-muted-foreground mt-2">
         Review your saved vocabulary.
       </p>
+      <Card>
+        <CardHeader>
+          <CardTitle>View Flashcards</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button type = "submit" className="w-full">View All Flashcards</Button>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Recently Added</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button type = "submit" className="w-full">View Recently Added</Button>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create New Flashcard</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button type = "submit" className="w-full">Create New</Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
+
